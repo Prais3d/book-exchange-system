19e84510520410de3ea0fbfb8de3850b0aab968c
Prais3d <saemon_36@hotmail.com>
2020-04-03 18:59:38 +0600

    first commit

---
+++ src/javaconnect.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author src7
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+
+public class javaconnect {
+    Connection conn;
+    public static Connection ConnecrDb() 
+    {
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:G:/book exchange system main/book exchange system main/bookexchange.sqlite");
+             return conn;  
+                    }catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    
+
+        }
+ }
+
